27bf3144697d03afff1ceb2ac41b3a1fb46cfbc9
frank.kang <frank.kang@coretronic.com>
2026-02-21 01:20:35 +0800

    feat: add utility functions for creating torch inputs and determining device

--- src/structcast_model/torch/trainer.py
+++ src/structcast_model/torch/trainer.py
@@ -1,18 +1,50 @@
+"""Trainer for PyTorch models."""
+
 from collections.abc import Callable
 from contextlib import AbstractContextManager, suppress
 from dataclasses import dataclass
+from logging import getLogger
 
+from pydantic import TypeAdapter, ValidationError
+from timm.utils import ModelEmaV3
 from torch.nn import Module
 
 from structcast_model.base_trainer import Any, BaseTrainer
 from structcast_model.torch.layers.criteria_tracker import CriteriaTracker
 from structcast_model.torch.types import Tensor
-from torch import no_grad
+from torch import cuda, float32, no_grad, rand
+
+logger = getLogger(__name__)
+
+
+def create_torch_inputs(shape: Any) -> Any:
+    """Create dummy inputs based on the provided shape."""
+    try:
+        return rand((1, *TypeAdapter(tuple[int, ...]).validate_python(shape)), dtype=float32)
+    except ValidationError:
+        pass
+    if isinstance(shape, dict):
+        return {k: create_torch_inputs(v) for k, v in shape.items()}
+    if isinstance(shape, (list, tuple)):
+        return [create_torch_inputs(v) for v in shape]
+    raise ValueError(f"Invalid tensor shape: {shape}")
+
+
+def get_torch_device(device: str | None = None) -> str:
+    """Get the device to run the model on."""
+    if device is None:
+        return "cuda" if cuda.is_available() else "cpu"
+    if device not in ["cpu", "cuda"]:
+        raise ValueError(f'Only "cpu" and "cuda" are supported. Got invalid device: {device}')
+    if device == "cuda" and not cuda.is_available():
+        logger.warning("CUDA is not available. Using CPU instead.")
+        return "cpu"
+    return device
 
 
 @dataclass(kw_only=True, slots=True)
-class TorchForward:
-    """A forward pass for a PyTorch model."""
+class TrainingStep:
+    """A training step for a PyTorch model."""
 
     losses: Module
     """A module that computes the losses for the model."""
@@ -35,6 +67,21 @@ class TorchForward:
 
 
 @dataclass(kw_only=True, slots=True)
+class ValidationStep(TrainingStep):
+    """A validation step for a PyTorch model."""
+
+    def __call__(self, inputs: dict[str, Any], model: Module) -> dict[str, Tensor]:
+        """Perform the forward pass for the given inputs and return the outputs and any additional information."""
+        with no_grad():
+            with self.autocast():
+                outputs = model(**inputs)
+                losses = self.losses(**inputs, **outputs)
+                if self.metrics is None:
+                    return losses
+                return {**losses, **self.metrics(**inputs, **outputs)}
+
+
+@dataclass(kw_only=True, slots=True)
 class TorchLogger:
     """A logger for PyTorch models."""
 
@@ -50,6 +97,18 @@ class TorchLogger:
         if self.metrics_tracker is not None:
             res.update(self.metrics_tracker({k: criteria[k] for k in self.metrics_tracker.criteria}))
         return {k: v.item() for k, v in res.items()}
+
+
+@dataclass(kw_only=True, slots=True)
+class TimmEmaWrapper:
+    """A wrapper for the EMA model from the timm library."""
+
+    ema: dict[str, ModelEmaV3]
+    """The EMA model."""
+
+    def __call__(self, **models: Module) -> dict[str, Any]:
+        """Return the EMA model."""
+        return {k: self.ema[k].module for k in models}
 
 
 @dataclass(kw_only=True)
