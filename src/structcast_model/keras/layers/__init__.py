f9f9ea1e7f0786647130e08f551a58b9c91d0315
frank.kang <frank.kang@coretronic.com>
2026-04-09 15:50:46 +0800

    fix: update import for GlobalResponseNormalization in layers module

--- src/structcast_model/keras/layers/__init__.py
+++ src/structcast_model/keras/layers/__init__.py
@@ -1,5 +1,5 @@
 """Layers module for Keras extensions of StructCast-Model."""
 
-from structcast_model.flax.layers.grn import GlobalResponseNorm
+from structcast_model.keras.layers.grn import GlobalResponseNormalization
 
-__all__ = ["GlobalResponseNorm"]
+__all__ = ["GlobalResponseNormalization"]
